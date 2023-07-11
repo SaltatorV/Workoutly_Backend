e805fee6450ea9ea48b2203f12495331b89931b1
saltatorv <saltatorv@gmail.com>
2023-07-11 22:19:11 +0200

    W:1 - Add monolith module

---
+++ monolith/src/main/java/com/workoutly/WorkoutlyBackendMonolithApplication.java
@@ -0,0 +1,11 @@
+package com.workoutly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutlyBackendMonolithApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WorkoutlyBackendMonolithApplication.class, args);
+    }
+}
