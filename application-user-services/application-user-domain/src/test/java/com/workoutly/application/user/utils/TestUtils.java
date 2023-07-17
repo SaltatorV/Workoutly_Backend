4403e10395b05a699c6c5e3be955f81e67cedfe3
saltatorv <saltatorv@gmail.com>
2023-07-18 01:10:44 +0200

    W:3 - application-user-domain - add Test Utils

---
+++ application-user-services/application-user-domain/src/test/java/com/workoutly/application/user/utils/TestUtils.java
@@ -0,0 +1,16 @@
+package com.workoutly.application.user.utils;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class TestUtils {
+    private static ObjectMapper mapper = new ObjectMapper();
+
+    public static String mapToString(Object object) {
+        try {
+            return mapper.writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
