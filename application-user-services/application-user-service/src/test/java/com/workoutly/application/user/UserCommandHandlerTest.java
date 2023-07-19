8a9dceb440de77e4d3d863a0b83483381c36042d
saltatorv <saltatorv@gmail.com>
2023-07-19 23:37:47 +0200

    W:4 - application-user-service - TDD: UserCommandHandler createUserTest

--- application-user-services/application-user-service/src/test/java/com/workoutly/application/user/UserCommandHandlerTest.java
+++ application-user-services/application-user-service/src/test/java/com/workoutly/application/user/UserCommandHandlerTest.java
@@ -1,4 +1,20 @@
 package com.workoutly.application.user;
 
+import org.junit.jupiter.api.Test;
+
 public class UserCommandHandlerTest {
+
+    private UserCommandHandler userCommandHandler = new UserCommandHandler();
+
+    @Test
+    public void createUserTest() {
+        //given
+        RegisterUserRequest request = new RegisterUserRequest("Username", "Example@example.pl", "Password", "Password");
+
+        //when
+        UserCreatedEvent event = userCommandHandler.createUser(request);
+
+        //then
+
+    }
 }
