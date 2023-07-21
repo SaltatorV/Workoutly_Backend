ca11a74244f9a0bf3a971ddf331b5482093d2011
saltatorv <saltatorv@gmail.com>
2023-07-22 01:45:22 +0200

    W:5 - application-user-service - TDD UserDataMapper first implementation for registerUserCommandToCommonUser method

--- application-user-services/application-user-service/src/main/java/com/workoutly/application/user/mapper/UserDataMapper.java
+++ application-user-services/application-user-service/src/main/java/com/workoutly/application/user/mapper/UserDataMapper.java
@@ -1,12 +1,16 @@
 package com.workoutly.application.user.mapper;
 
 import com.workoutly.application.user.User;
+import com.workoutly.application.user.VO.UserRole;
 import com.workoutly.application.user.dto.command.RegisterUserCommand;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserDataMapper {
     public User registerUserCommandToCommonUser(RegisterUserCommand registerUserCommand) {
-        return null;
+        return new User(registerUserCommand.getUsername(),
+                registerUserCommand.getPassword(),
+                registerUserCommand.getEmail(),
+                UserRole.COMMON);
     }
 }
