1ef130cf6ca40264813bfa2ffb494b82af05f0b3
saltatorv <saltatorv@gmail.com>
2023-07-24 23:18:36 +0200

    W:7 - application-user-application - TDD RegisterController - add validation constraints for RegisterUserCommand

--- application-user-services/application-user-service/src/main/java/com/workoutly/application/user/dto/command/RegisterUserCommand.java
+++ application-user-services/application-user-service/src/main/java/com/workoutly/application/user/dto/command/RegisterUserCommand.java
@@ -1,13 +1,26 @@
 package com.workoutly.application.user.dto.command;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class RegisterUserCommand {
+    @NotNull
+    @Size(min = 3, max = 20)
     private String username;
+
+    @Email
+    @NotNull
     private String email;
+
+    @NotEmpty
     private String password;
+
+    @NotEmpty
     private String confirmPassword;
 }
