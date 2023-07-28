5d29ebab92ae5843ac387460e9a40aed24582f0a
saltatorv <saltatorv@gmail.com>
2023-07-29 01:19:21 +0200

    W:10 - application-user-service - UserDataMapper - add test for userCreatedEventToRegisterUserResponse

--- application-user-services/application-user-service/src/test/java/com/workoutly/application/user/mapper/UserDataMapperTest.java
+++ application-user-services/application-user-service/src/test/java/com/workoutly/application/user/mapper/UserDataMapperTest.java
@@ -4,6 +4,8 @@ import com.workoutly.application.user.User;
 import com.workoutly.application.user.VO.UserRole;
 import com.workoutly.application.user.VO.UserSnapshot;
 import com.workoutly.application.user.dto.command.RegisterUserCommand;
+import com.workoutly.application.user.dto.response.RegisterUserResponse;
+import com.workoutly.application.user.event.UserCreatedEvent;
 import org.junit.jupiter.api.Test;
 
 import static com.workoutly.application.user.builder.RegisterUserCommandBuilder.aRegisterUserCommand;
@@ -32,6 +34,25 @@ public class UserDataMapperTest {
 
     }
 
+    @Test
+    public void testUserCreatedEventToRegisterUserResponse() {
+        //given
+        var command = aRegisterUserCommand()
+                .withUsername("Test")
+                .withPassword("Password")
+                .withConfirmPassword("Password")
+                .withEmailAddress("Example@example.pl")
+                .create();
+
+        var event = new UserCreatedEvent(createCommonUserSnapshot(command));
+
+        //when
+        var response = userDataMapper.userCreatedEventToRegisterUserResponse(event);
+
+        //then
+        assertIsResponseValid(response, event);
+    }
+
     private void assertIsCommonUserValid(User user, RegisterUserCommand command) {
         UserSnapshot snapshot = user.createSnapshot();
         UserSnapshot commandSnapshot = createCommonUserSnapshot(command);
@@ -41,5 +62,9 @@ public class UserDataMapperTest {
     private UserSnapshot createCommonUserSnapshot(RegisterUserCommand command) {
         return new User(command.getUsername(), command.getPassword(), command.getEmail(), UserRole.COMMON)
                 .createSnapshot();
+    }
+
+    private void assertIsResponseValid(RegisterUserResponse response, UserCreatedEvent event) {
+        assertEquals(event.getSnapshot().getUsername(), response.getUsername());
     }
 }
