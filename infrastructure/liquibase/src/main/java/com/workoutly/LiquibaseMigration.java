1ff2e680702780c6db70290dedaaee8d61021658
saltatorv <saltatorv@gmail.com>
2023-08-01 01:11:08 +0200

    W:17 - Liquibase - add dependency configuration

---
+++ infrastructure/liquibase/src/main/java/com/workoutly/LiquibaseMigration.java
@@ -0,0 +1,11 @@
+package com.workoutly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseMigration {
+    public static void main(String[] args) {
+        SpringApplication.run(LiquibaseMigration.class, args);
+    }
+}
