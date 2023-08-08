60f97aeb8673637dc6c46ce2d496d272d201d32b
saltatorv <saltatorv@gmail.com>
2023-08-08 17:10:43 +0200

    W:28 - application-user-domain - UserDomainService add change email and password methods to interface

--- application-user-services/application-user-domain/src/main/java/com/workoutly/application/user/UserDomainService.java
+++ application-user-services/application-user-domain/src/main/java/com/workoutly/application/user/UserDomainService.java
@@ -1,10 +1,16 @@
 package com.workoutly.application.user;
 
+import com.workoutly.application.user.VO.UserSnapshot;
 import com.workoutly.application.user.event.UserActivatedEvent;
 import com.workoutly.application.user.event.UserCreatedEvent;
+import com.workoutly.application.user.event.UserUpdateEvent;
 
 public interface UserDomainService {
     UserCreatedEvent initializeUser(User user);
 
     UserActivatedEvent activateUser(User user);
+
+    UserUpdateEvent changeEmail(UserSnapshot userSnapshot);
+
+    UserUpdateEvent changePassword(UserSnapshot userSnapshot);
 }
