cfd410379fd23d1c22b3bbf6584bdd5602cf3c71
saltatorv <saltatorv@gmail.com>
2023-08-15 15:28:09 +0200

    W:32 - measurement-services-domain - add domain exception

---
+++ measurement-services/measurement-domain/src/main/java/com/workoutly/measurement/exception/BodyMeasurementsDomainException.java
@@ -0,0 +1,9 @@
+package com.workoutly.measurement.exception;
+
+import com.workoutly.common.exception.DomainException;
+
+public class BodyMeasurementsDomainException extends DomainException {
+    public BodyMeasurementsDomainException(String message) {
+        super(message);
+    }
+}
