d58b555f1396c1d3e7557d3dfd3fbff9a11a2c0f
saltatorv <saltatorv@gmail.com>
2023-08-23 16:34:41 +0200

    W:35 - measurement-service - add MeasurementDataMapper

---
+++ measurement-services/measurement-service/src/main/java/com/workoutly/measurement/mapper/MeasurementDataMapper.java
@@ -0,0 +1,10 @@
+package com.workoutly.measurement.mapper;
+
+import com.workoutly.measurement.BodyMeasurement;
+import com.workoutly.measurement.dto.command.BodyMeasurementCommand;
+
+public class MeasurementDataMapper {
+    public BodyMeasurement mapBodyMeasurementCommandToBodyMeasurement(BodyMeasurementCommand command) {
+        return null;
+    }
+}
