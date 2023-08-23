06139f5514f553cb5053fb887b0f0cef78dfc0c5
saltatorv <saltatorv@gmail.com>
2023-08-23 21:04:27 +0200

    W:35 - measurement-service - AuthenticationProvider::getAuthenticatedUser remove static type.

--- measurement-services/measurement-service/src/main/java/com/workoutly/measurement/auth/AuthenticationProvider.java
+++ measurement-services/measurement-service/src/main/java/com/workoutly/measurement/auth/AuthenticationProvider.java
@@ -2,9 +2,11 @@ package com.workoutly.measurement.auth;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 
+@Component
 public class AuthenticationProvider {
-    public static String getAuthenticatedUser() {
+    public String getAuthenticatedUser() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         return authentication.getName();
     }
