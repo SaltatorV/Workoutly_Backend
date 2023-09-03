861dbe71a3522073fab169601d4c5899c5533094
saltatorv <saltatorv@gmail.com>
2023-09-04 00:25:33 +0200

    W:38 - measurement-service - create MeasurementDataMapper::mapBodyMeasurementCreatedEventToResponse

--- measurement-services/measurement-service/src/main/java/com/workoutly/measurement/mapper/MeasurementDataMapper.java
+++ measurement-services/measurement-service/src/main/java/com/workoutly/measurement/mapper/MeasurementDataMapper.java
@@ -2,9 +2,15 @@ package com.workoutly.measurement.mapper;
 
 import com.workoutly.measurement.BodyMeasurement;
 import com.workoutly.measurement.dto.command.BodyMeasurementCommand;
+import com.workoutly.measurement.dto.response.MessageResponse;
+import com.workoutly.measurement.event.BodyMeasurementCreatedEvent;
 
 public class MeasurementDataMapper {
     public BodyMeasurement mapBodyMeasurementCommandToBodyMeasurement(BodyMeasurementCommand command) {
+        return null;
+    }
+
+    public MessageResponse mapBodyMeasurementCreatedEventToResponse(BodyMeasurementCreatedEvent event) {
         return null;
     }
 }
