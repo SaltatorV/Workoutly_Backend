8c8a95dd2977c5ae1f07c86f116c478d26a9c0f0
saltatorv <saltatorv@gmail.com>
2023-09-03 23:50:21 +0200

    W:37 - measurement-service - create BodyMeasurementsPageQuery

---
+++ measurement-services/measurement-service/src/main/java/com/workoutly/measurement/dto/query/BodyMeasurementsPageQuery.java
@@ -0,0 +1,13 @@
+package com.workoutly.measurement.dto.query;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BodyMeasurementsPageQuery {
+
+    private int page;
+}
