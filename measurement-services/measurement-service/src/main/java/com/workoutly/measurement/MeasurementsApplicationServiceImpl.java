c9e3fc3c90e4406fd7562e4b3ec08d0e7eacbeb6
saltatorv <saltatorv@gmail.com>
2023-09-05 23:29:45 +0200

    W:38 - measurement-service - MeasurementsApplicationServiceImpl::createBodyMeasurement - implementation

--- measurement-services/measurement-service/src/main/java/com/workoutly/measurement/MeasurementsApplicationServiceImpl.java
+++ measurement-services/measurement-service/src/main/java/com/workoutly/measurement/MeasurementsApplicationServiceImpl.java
@@ -1,6 +1,24 @@
 package com.workoutly.measurement;
 
+import com.workoutly.measurement.dto.command.BodyMeasurementCommand;
+import com.workoutly.measurement.dto.response.MessageResponse;
+import com.workoutly.measurement.event.BodyMeasurementCreatedEvent;
+import com.workoutly.measurement.mapper.MeasurementDataMapper;
 import com.workoutly.measurement.port.input.MeasurementsApplicationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
 
+@Service
+@Validated
+@RequiredArgsConstructor
 public class MeasurementsApplicationServiceImpl implements MeasurementsApplicationService {
+    private final BodyMeasurementCommandHandler bodyMeasurementCommandHandler;
+    private final MeasurementDataMapper mapper;
+
+    public MessageResponse createBodyMeasurement(BodyMeasurementCommand command) {
+        BodyMeasurementCreatedEvent event = bodyMeasurementCommandHandler.createBodyMeasurement(command);
+
+        return mapper.mapBodyMeasurementCreatedEventToResponse(event);
+    }
 }
--- measurement-services/measurement-service/src/test/java/com/workoutly/measurement/MeasurementsApplicationServiceImplTest.java
+++ measurement-services/measurement-service/src/test/java/com/workoutly/measurement/MeasurementsApplicationServiceImplTest.java
@@ -1,4 +1,91 @@
 package com.workoutly.measurement;
 
+import com.workoutly.measurement.VO.BodyMeasurementId;
+import com.workoutly.measurement.VO.BodyMeasurementSnapshot;
+import com.workoutly.measurement.dto.command.BodyMeasurementCommand;
+import com.workoutly.measurement.dto.response.MessageResponse;
+import com.workoutly.measurement.event.BodyMeasurementCreatedEvent;
+import com.workoutly.measurement.mapper.MeasurementDataMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.Instant;
+import java.util.Date;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doReturn;
+
+@ExtendWith(MockitoExtension.class)
 public class MeasurementsApplicationServiceImplTest {
+    @Mock
+    private BodyMeasurementCommandHandler bodyMeasurementCommandHandler;
+    @Mock
+    private MeasurementDataMapper mapper;
+    @InjectMocks
+    private MeasurementsApplicationServiceImpl service;
+
+    @Test
+    private void testCreateBodyMeasurement() {
+        //given
+        var command = createSampleBodyMeasurementCommand();
+        var event = createBodyMeasurementEvent(command);
+        var message = new MessageResponse("Body measurement created");
+
+        doReturn(event)
+                .when(bodyMeasurementCommandHandler)
+                .createBodyMeasurement(command);
+
+        doReturn(message)
+                .when(mapper)
+                .mapBodyMeasurementCreatedEventToResponse(event);
+
+
+        //when
+        var response = service.createBodyMeasurement(command);
+
+        //then
+        assertEquals(message, response);
+    }
+
+    private BodyMeasurementCommand createSampleBodyMeasurementCommand() {
+        return new BodyMeasurementCommand(
+                10,
+                11,
+                12,
+                13,
+                14,
+                15,
+                16,
+                17,
+                18,
+                19,
+                20,
+                Date.from(Instant.now())
+        );
+    }
+
+    private BodyMeasurementCreatedEvent createBodyMeasurementEvent(BodyMeasurementCommand command) {
+        BodyMeasurementSnapshot snapshot = new BodyMeasurementSnapshot(
+                new BodyMeasurementId(UUID.randomUUID()),
+                command.getNeck(),
+                command.getChest(),
+                command.getLeftBiceps(),
+                command.getRightBiceps(),
+                command.getLeftForearm(),
+                command.getRightForearm(),
+                command.getWaist(),
+                command.getLeftThigh(),
+                command.getRightThigh(),
+                command.getLeftCalf(),
+                command.getRightCalf(),
+                command.getDate(),
+                "test"
+        );
+
+        return new BodyMeasurementCreatedEvent(snapshot);
+    }
 }
