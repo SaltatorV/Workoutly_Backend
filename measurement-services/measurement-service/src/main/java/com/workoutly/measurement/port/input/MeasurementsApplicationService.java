ac67bb6f5090a0df3e569c7918b7dfeba2958016
saltatorv <saltatorv@gmail.com>
2023-09-11 13:15:03 +0200

    W:X - measurement-service - MeasurementsApplicationService port - add methods to implement

--- measurement-services/measurement-service/src/main/java/com/workoutly/measurement/port/input/MeasurementsApplicationService.java
+++ measurement-services/measurement-service/src/main/java/com/workoutly/measurement/port/input/MeasurementsApplicationService.java
@@ -1,4 +1,19 @@
 package com.workoutly.measurement.port.input;
 
+import com.workoutly.measurement.dto.command.BodyMeasurementCommand;
+import com.workoutly.measurement.dto.command.BodyMeasurementDeleteCommand;
+import com.workoutly.measurement.dto.query.BodyMeasurementsPageQuery;
+import com.workoutly.measurement.dto.response.BodyMeasurementsResponse;
+import com.workoutly.measurement.dto.response.MessageResponse;
+
 public interface MeasurementsApplicationService {
+    MessageResponse createBodyMeasurement(BodyMeasurementCommand command;
+
+    MessageResponse updateBodyMeasurement(BodyMeasurementCommand command);
+
+    MessageResponse deleteBodyMeasurement(BodyMeasurementDeleteCommand command);
+
+    BodyMeasurementsResponse findSummaryBodyMeasurements();
+
+    BodyMeasurementsResponse findBodyMeasurementsByPage(BodyMeasurementsPageQuery query);
 }
