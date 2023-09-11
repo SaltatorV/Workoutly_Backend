458dd2c01c8cbae5f3263a9ed0dc23cbad40ae71
saltatorv <saltatorv@gmail.com>
2023-09-11 18:47:38 +0200

    W:44 - measurement-dataaccess - create MeasurementRepository adapter

---
+++ measurement-services/measurement-dataaccess/src/main/java/com/workoutly/adapter/MeasurementRepositoryImpl.java
@@ -0,0 +1,40 @@
+package com.workoutly.adapter;
+
+import com.workoutly.measurement.VO.BodyMeasurementSnapshot;
+import com.workoutly.measurement.port.output.MeasurementRepository;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+public class MeasurementRepositoryImpl implements MeasurementRepository {
+    @Override
+    public void saveBodyMeasurement(BodyMeasurementSnapshot snapshot) {
+
+    }
+
+    @Override
+    public boolean checkBodyMeasurementExists(Date date, String authenticatedUser) {
+        return false;
+    }
+
+    @Override
+    public Optional<BodyMeasurementSnapshot> findBodyMeasurementSnapshot(Date date, String authenticatedUser) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void deleteBodyMeasurementByDate(Date date, String authenticatedUser) {
+
+    }
+
+    @Override
+    public Optional<List<BodyMeasurementSnapshot>> findSummaryBodyMeasurements(String authenticatedUser) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<List<BodyMeasurementSnapshot>> findBodyMeasurementsByPage(int page, String authenticatedUser) {
+        return Optional.empty();
+    }
+}
