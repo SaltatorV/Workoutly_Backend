b2bff449a6ed4a78cf874c350e40badcc4423695
saltatorv <saltatorv@gmail.com>
2023-09-21 00:02:00 +0200

    W:47 - MeasurementDomainService add updateBodyWeight method to implement

--- measurement-services/measurement-domain/src/main/java/com/workoutly/measurement/MeasurementDomainService.java
+++ measurement-services/measurement-domain/src/main/java/com/workoutly/measurement/MeasurementDomainService.java
@@ -1,12 +1,16 @@
 package com.workoutly.measurement;
 
 import com.workoutly.measurement.VO.BodyMeasurementSnapshot;
+import com.workoutly.measurement.VO.BodyWeightSnapshot;
 import com.workoutly.measurement.event.BodyMeasurementCreatedEvent;
 import com.workoutly.measurement.event.BodyMeasurementUpdatedEvent;
 import com.workoutly.measurement.event.BodyWeightCreatedEvent;
+import com.workoutly.measurement.event.BodyWeightUpdatedEvent;
 
 public interface MeasurementDomainService {
     BodyWeightCreatedEvent initializeBodyWeight(BodyWeight bodyWeight, String username);
+
+    BodyWeightUpdatedEvent updateBodyWeight(BodyWeight bodyWeightToUpdate, BodyWeightSnapshot snapshot);
 
     BodyMeasurementCreatedEvent initializeBodyMeasurement(BodyMeasurement bodyMeasurement, String username);
 
